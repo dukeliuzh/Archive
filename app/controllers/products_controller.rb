ff3db12a58efaffa9bcdb8de0842ab9650d2dfe9
Nina Huang <ninahuang@Ninas-iMac.local>
2016-04-28 22:07:08 -0700

    add checkout

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,4 +9,10 @@ class ProductsController < ApplicationController
     @product = Shoppe::Product.root.find_by_permalink(params[:permalink])
   end
 
+  def buy
+  	@product = Shoppe::Product.root.find_by_permalink!(params[:permalink])
+  	current_order.order_items.add_item(@product, 1)
+  	redirect_to product_path(@product.permalink), :notice => "Product has been added successfuly!"
+  end
+
 end
